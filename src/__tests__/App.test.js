327f312dea88c8dc7309f279775bbe5b9b7022f2
Zoe Guidry <oyezoey@gmail.com>
2022-11-05 14:14:41 -0500

    commit tree 7609da488747403e35176f3f5b7784fb37f737b7

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -14,7 +14,7 @@ test("renders the correct child components", () => {
     Uncomment the line below to see the DOM elements being returned
     by the App component in your terminal when you run the tests
   */
-  // screen.debug();
+  screen.debug();
 
   expect(container.querySelector("nav")).toBeInTheDocument();
   expect(container.querySelector("#home")).toBeInTheDocument();
